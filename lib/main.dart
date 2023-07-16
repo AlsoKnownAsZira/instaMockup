30ef3cd254fe1a24686f64329e4278863965e264
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-07-17 01:26:16 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(instaApp());
+}
+
+class instaApp extends StatelessWidget {
+  const instaApp({super.key});
+
+  @override
+  Widget build (BuildContext context) {
+    return const  Scaffold();
+  }
+}
