e0c9f0382c6da9991aa2e22a5f56636ffe8dcdd6
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-07-17 02:58:47 +0700

    creating assets folder and adding pp

--- lib/pages/profilePage.dart
+++ lib/pages/profilePage.dart
@@ -47,7 +47,38 @@ class profilePage extends StatelessWidget {
               )),
         ],
       ),
-    
+      body: ListView(
+        children: [
+          Row(
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: 120,
+                    height: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(60),
+                        gradient: LinearGradient(colors: [
+                          Colors.orange,
+                          Colors.red,
+                          Colors.purple
+                        ])),
+                  ),
+                  Container(
+                      width: 110,
+                      height: 110,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 5, color: Colors.white),
+                        borderRadius: BorderRadius.circular(60),
+                       image: DecorationImage(image: AssetImage('assets/images/ppzira.jpg'))
+                      ))
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
