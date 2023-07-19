e79bab4e33431ef55e08e44738b6c626ecebd56e
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-07-20 06:29:27 +0700

    arranging buttons

--- lib/widgets/postDetail.dart
+++ lib/widgets/postDetail.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 
 class postDetail extends StatelessWidget {
   const postDetail(
-      {required this.imageList, required this.selectedIndex, required this.imageCapt, required this.likeCount, super.key});
+      {required this.imageList,
+      required this.selectedIndex,
+      required this.imageCapt,
+      required this.likeCount,
+      super.key});
   final List<String> imageList;
   final int selectedIndex;
   final List<String> imageCapt;
@@ -20,7 +24,7 @@ class postDetail extends StatelessWidget {
               Icons.arrow_back_ios_rounded,
               color: (Colors.black),
             )),
-        title: Text("Posts"),
+        title: const Text("Posts"),
         foregroundColor: Colors.black,
         centerTitle: false,
         backgroundColor: Colors.white,
@@ -31,31 +35,60 @@ class postDetail extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(10),
             child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Row( 
-              
+                Row(
                   children: [
-                Container(
-                       width: 50,
-                      height: 50,
-                      decoration: BoxDecoration(
-                          border: Border.all(width: 3, color: Colors.white),
-                          borderRadius: BorderRadius.circular(60),
-                          image: DecorationImage(
-                            image: AssetImage("assets/images/ppzira.jpg"),
-                            fit: BoxFit.cover,
-                          ))
+                    Container(
+                        width: 50,
+                        height: 50,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 3, color: Colors.white),
+                            borderRadius: BorderRadius.circular(60),
+                            image:const DecorationImage(
+                              image: AssetImage("assets/images/ppzira.jpg"),
+                              fit: BoxFit.cover,
+                            ))),
+                 const   SizedBox(
+                      width: 5,
+                    ),
+                    Text("Ziraaa_")
+                  ],
                 ),
-                SizedBox(width: 5,),
-                Text("Ziraaa_")
-                    
-                ],),
-                Icon(Icons.more_vert_outlined)
+               const Icon(Icons.more_vert_outlined)
               ],
             ),
           ),
-      
+          Container(
+            width: double.infinity,
+            height: 400,
+            decoration: BoxDecoration(
+                color: Colors.black,
+                image: DecorationImage(
+                    image: AssetImage(imageList[selectedIndex]),
+                    fit: BoxFit.cover)),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Row(
+              children: [
+                Row(
+                  children: [
+                    IconButton(onPressed: (){
+
+                    }, icon: Icon(Icons.favorite_border)),
+                  
+                    Icon(Icons.mode_comment_outlined),
+                      SizedBox(width: 10,),
+                    Icon(Icons.share)
+                  ],
+                
+                ),
+                Icon(Icons.bookmark_border)
+              ],
+            ),
+          ),
+         
         ],
       ),
     );
