80d95e0789f9e01ee68d2ce466f1d5410a11ff04
Brilyan Satria Wahyuda <brilyansatria10@gmail.com>
2023-08-01 00:37:37 +0700

    creating homePost for creating post on homepage

---
+++ lib/widgets/homePost.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class homePost extends StatelessWidget {
+  const homePost({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      children: [
+        
+      ],
+    );
+  }
+}
